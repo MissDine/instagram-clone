47c6a445815f98b789817165029a041e0954df91
Enid Nyatichi <enidnyatichi1212@gmail.com>
2022-10-20 19:15:29 +0300

    Feature edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+ root to: "public#homepage" #route is public and the method is homepage
+
+ end
   # root "articles#index"
 end
